51ea79d9c13bc513c6303ca27247a31ebd944740
giuseppe_difilippo <giuseppe.difilippo@live.it>
2023-09-28 11:40:04 +0200

    update

--- es1.go
+++ es1.go
@@ -6,7 +6,9 @@ func main() {
 	var count int
 	nums := []int{9, 1, 3, 5, 2, 0, 8}
 	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
 
+		}
 	}
 	fmt.Println(count)
 }
--- es2.go
+++ es2.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	var stringa string
 	var a, count int
+	stringa = "aaaccccbb"
 	for _, x := range stringa {
 		if string(x) == "a" {
 			a++
